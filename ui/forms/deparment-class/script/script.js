3c43b6f32fa45d2160577bc0f0635253c0fc7ce5
WRBKOR23 <haijdk2433@gmail.com>
2021-02-16 07:53:02 +0700

    edit way to create table-class. add method to send notification to api. add composer in order to connect with firebase

--- ui/forms/deparment-class/script/script.js
+++ ui/forms/deparment-class/script/script.js
@@ -1,53 +1,91 @@
+let sender
+const fieldList = {
+    title: 'Tiêu đề',
+    content: 'Nội dung',
+    typez: 'Loại thông báo'
+}
 const checkBox = document.querySelectorAll('input')
 let checkBoxes1 = document.getElementsByName('academic_year')
 let checkBoxes2 = document.getElementsByName('faculty')
 let allClass = []
 let selectedClass = []
-let academic_year = []
-let faculty = []
-let conditions = []
+let academicYears = []
+let faculties = []
 
-fetch('../../../api-v2/manage/get_department_class.php')
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (responseAsJson) {
-        allClass = responseAsJson
-    })
-    .catch(function (error) {
-        console.log('Looks like there was a problem: \n', error)
-    })
+async function prepare() {
+    document.getElementById('all_academic_year').addEventListener('click', tickAllForAcademic_YearAndFaculty)
+    document.getElementById('all_faculty').addEventListener('click', tickAllForAcademic_YearAndFaculty)
+    document.getElementsByName('button')[0].addEventListener('click', trySendNotification)
 
+    await fetchData();
 
-for (let i = 0; i < checkBox.length; i++) {
-    checkBox[i].addEventListener('change', function () {
-        if (!this.checked && this.value !== 'all') {
-            document.getElementsByClassName(this.name)[0].checked = false
-        }
-        else if (this.checked && this.value !== 'all') {
-            let checkBoxes = document.getElementsByName(this.name)
-            let flag = true
-            for (let j = 0; j < checkBoxes.length; j++) {
-                if (!checkBoxes[j].checked) {
-                    flag = false
-                    break
-                }
-            }
-            document.getElementsByClassName(this.name)[0].checked = flag
-        }
+    addEventForAcademic_YearAndFaculty()
 
-        if (!isChecked()) {
-            reset()
-            return
-        }
-
-        getConditions()
-    })
+    sender = await getSender()
 }
 
-function reset() {
-    document.querySelector('.class').innerHTML = ''
-    selectedClass = []
+function tickAllForAcademic_YearAndFaculty() {
+    let checkBoxes = document.getElementsByName(this.classList[0])
+    for (let i = 0; i < checkBoxes.length; i++) {
+        if (checkBoxes[i].checked !== this.checked) {
+            checkBoxes[i].checked = this.checked
+        }
+    }
+}
+
+async function fetchData() {
+    try {
+        const baseUrl = '../../../api-v2/manage/get_department_class.php'
+        const init = {
+            method: 'GET',
+            cache: 'no-cache'
+        }
+        let response = await fetch(baseUrl, init)
+        allClass = await response.json()
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+function addEventForAcademic_YearAndFaculty() {
+    for (let i = 1; i < checkBox.length; i++) {
+        checkBox[i].addEventListener('change', function () {
+            if (!this.checked && this.value !== 'all') {
+                document.getElementsByClassName(this.name)[0].checked = false
+            }
+            else if (this.checked && this.value !== 'all') {
+                let checkBoxes = document.getElementsByName(this.name)
+                let flag = true
+                for (let j = 0; j < checkBoxes.length; j++) {
+                    if (!checkBoxes[j].checked) {
+                        flag = false
+                        break
+                    }
+                }
+                document.getElementsByClassName(this.name)[0].checked = flag
+            }
+
+            if (!isChecked()) {
+                reset()
+                return
+            }
+
+            getConditions()
+        })
+    }
+}
+
+async function getSender() {
+    const baseUrl = '../../shared/session.php?var=department_id'
+    const init = {
+        method: 'GET'
+    }
+
+    let response = await fetch(baseUrl, init)
+    let responseAsJson = await response.json()
+
+    return responseAsJson
 }
 
 function isChecked() {
@@ -69,174 +107,233 @@ function isChecked() {
     return (flag1 && flag2)
 }
 
-function tickAll(src) {
-    let checkBoxes = document.getElementsByName(src.classList[0])
-    for (let i = 0; i < checkBoxes.length; i++) {
-        if (checkBoxes[i].checked !== src.checked) {
-            checkBoxes[i].checked = src.checked
-        }
-    }
+function reset() {
+    document.querySelector('.class').innerHTML = ''
+    selectedClass = []
 }
 
-
 function getConditions() {
-    academic_year = []
-    faculty = []
-    conditions = []
+    academicYears = []
+    faculties = []
 
     if (all_academic_year.checked) {
         if (all_faculty.checked) {
-            academic_year = ['K54', 'K55', 'K56', 'K57', 'K58', 'K59', 'K60']
-            faculty = ['CK', 'CNTT', 'CT', 'DDT', 'GDQP', 'GDTC', 'KHCB', 'KTXD', 'LLCT', 'VTKT']
+            academicYears = ['K54', 'K55', 'K56', 'K57', 'K58', 'K59', 'K60']
+            faculties = ['CK', 'CNTT', 'CT', 'DDT', 'GDQP', 'GDTC', 'KHCB', 'KTXD', 'LLCT', 'VTKT']
         }
         else {
-            academic_year = ['K54', 'K55', 'K56', 'K57', 'K58', 'K59', 'K60']
+            academicYears = ['K54', 'K55', 'K56', 'K57', 'K58', 'K59', 'K60']
 
             for (let i = 0; i < checkBoxes2.length; i++) {
                 if (checkBoxes2[i].checked) {
-                    faculty.push(checkBoxes2[i].value)
+                    faculties.push(checkBoxes2[i].value)
                 }
             }
         }
     }
     else {
         if (all_faculty.checked) {
-            faculty = ['CK', 'CNTT', 'CT', 'DDT', 'GDQP', 'GDTC', 'KHCB', 'KTXD', 'LLCT', 'VTKT']
+            faculties = ['CK', 'CNTT', 'CT', 'DDT', 'GDQP', 'GDTC', 'KHCB', 'KTXD', 'LLCT', 'VTKT']
 
             for (let i = 0; i < checkBoxes1.length; i++) {
                 if (checkBoxes1[i].checked) {
-                    academic_year.push(checkBoxes1[i].value)
+                    academicYears.push(checkBoxes1[i].value)
                 }
             }
         }
         else {
             for (let i = 0; i < checkBoxes1.length; i++) {
                 if (checkBoxes1[i].checked) {
-                    academic_year.push(checkBoxes1[i].value)
+                    academicYears.push(checkBoxes1[i].value)
                 }
             }
 
             for (let i = 0; i < checkBoxes2.length; i++) {
                 if (checkBoxes2[i].checked) {
-                    faculty.push(checkBoxes2[i].value)
+                    faculties.push(checkBoxes2[i].value)
                 }
-            }
-
-            if (Object.entries(academic_year).length === 0 || Object.entries(faculty).length === 0) {
-                return
             }
         }
     }
-    conditions = [academic_year, faculty]
 
     getClass()
 }
 
 function getClass() {
-    let classWithConditions = []
+    let data = {}
 
-    for (let i = 0; i < allClass.length; i++) {
-        if (conditions[0].lastIndexOf(allClass[i]['Academic_Year']) !== -1 &&
-            conditions[1].lastIndexOf(allClass[i]['ID_Faculty']) !== -1) {
-            classWithConditions.push([allClass[i]['ID_Class'], allClass[i]['Academic_Year'], allClass[i]['ID_Faculty']])
+    for (let _class of allClass) {
+        let {Academic_Year: academicYearOfClass, ID_Faculty: facultyOfClass, ID_Class: idOfClass} = _class
+
+        if (academicYears.lastIndexOf(academicYearOfClass) !== -1 &&
+            faculties.lastIndexOf(facultyOfClass) !== -1) {
+
+            if (data[academicYearOfClass] === undefined) {
+                data[academicYearOfClass] = {}
+            }
+
+            if (data[academicYearOfClass][facultyOfClass] === undefined) {
+                data[academicYearOfClass][facultyOfClass] = []
+            }
+
+            data[academicYearOfClass][facultyOfClass].push(idOfClass)
         }
+        createTable(data)
     }
-    createTable(classWithConditions)
 }
-
-/*
-    function createTable()
-    @data[x][0] return value of ID_Class
-    @data[x][0] return value of Academic_Year
-    @data[x][0] return value of ID_Faculty
- */
 
 function createTable(data) {
     reset()
 
-    if (data.length === 0) {
+    if (Object.entries(data).length === 0) {
         return
     }
 
-    let html = '<legend>' + data[0][1] + '</legend><table>'
-    let tempAcademmic_Year = data[0][1]
-    let tempFaculty = ''
-    let j = 0
+    for (const [_academicYear, _faculty] of Object.entries(data)) {
+        let area = createAcademicYearArea(_academicYear)
+        let innerTable = createInnerTable()
 
-    for (let i = 0; i < data.length; i++) {
-        if (data[i][1] !== tempAcademmic_Year) {
-            for (; j < 8; j++) {
-                html += '<td></td>'
+        for (const [facultyElement, __class] of Object.entries(_faculty)) {
+            let rowTag = createRow()
+            let columnFacultyTag = createColumn()
+            let facultyTag = createFacultyTag(facultyElement)
+            let checkAllTag = createCheckAllTag(_academicYear, facultyElement)
+            let columnCheckAllTag = createColumn()
+
+            columnFacultyTag.appendChild(facultyTag)
+            rowTag.appendChild(columnFacultyTag)
+
+            columnCheckAllTag.appendChild(checkAllTag)
+            rowTag.appendChild(columnCheckAllTag)
+
+            let counter = 2;
+            for (const _singleClass of __class) {
+                selectedClass.push(_singleClass)
+
+                let classTag = createClassTag(_singleClass, _academicYear, facultyElement)
+                let columnClassTag = createColumn()
+
+                if (counter >= 8) {
+                    innerTable.appendChild(rowTag)
+
+                    rowTag = createRow()
+
+                    rowTag.appendChild(createColumn())
+                    rowTag.appendChild(createColumn())
+
+                    counter = 2
+                }
+                columnClassTag.appendChild(classTag)
+                rowTag.appendChild(columnClassTag)
+
+                counter++;
             }
-            j = 0
+            if (counter < 7) {
+                for (; counter < 8; counter++) {
+                    rowTag.appendChild(createColumn())
+                }
+            }
 
-            html += '</tr>'
-            html += '</table><legend>' + data[i][1] + '</legend><table>'
-            tempAcademmic_Year = data[i][1]
-            tempFaculty = ''
+            innerTable.appendChild(rowTag)
         }
 
-        if (j === 0) {
-            if (tempFaculty === data[i][2]) {
-                html += '<td></td>'
-                j++
-            }
-            else {
-                tempFaculty = data[i][2]
-                html += '<tr><td><div class="form-check form-check-inline">' + tempFaculty + '</div></td>'
-                html += '<td><div class="form-check form-check-inline">'
-                html += '<input type="checkbox" '
-                html += 'class="' + tempAcademmic_Year + tempFaculty + ' form-check-input"'
-                html += 'id="' + tempAcademmic_Year + tempFaculty + '" '
-                html += ' value="all" '
-                html += '" onclick="tickAllForClass(this)">'
-                html += '<label for="' + tempAcademmic_Year + tempFaculty + '" '
-                html += 'class="form-check-label">Chọn tất cả</label>'
-                html += '</div></td>'
-                j += 2
-            }
-        }
-
-        if (data[i][2] !== tempFaculty) {
-            tempFaculty = data[i][2]
-
-            for (; j < 8; j++) {
-                html += '<td></td>'
-            }
-
-            html += '<tr><td><div class="form-check form-check-inline">' + tempFaculty + '</div></td>'
-            html += '<td><div class="form-check form-check-inline">'
-            html += '<input type="checkbox" '
-            html += 'class="' + tempAcademmic_Year + tempFaculty + ' form-check-input"'
-            html += 'id="' + tempAcademmic_Year + tempFaculty + '" '
-            html += 'value="all" '
-            html += '" onclick="tickAllForClass(this)">'
-            html += '<label for="' + tempAcademmic_Year + tempFaculty + '" '
-            html += 'class="form-check-label">Chọn tất cả</label>'
-            html += '</div></td>'
-            j = 2
-        }
-
-        html += '<td><div class="form-check form-check-inline">'
-        html += '<input type="checkbox" '
-        html += 'class="' + tempAcademmic_Year + tempFaculty + '" '
-        html += 'id="' + data[i][0] + '"'
-        html += 'name="' + tempAcademmic_Year + tempFaculty + '" '
-        html += 'value="' + data[i][0] + '">'
-        html += '<label for="' + data[i][0] + '" '
-        html += 'class="form-check-label">' + data[i][0] + '</label></div></td>'
-
-        if (j === 7) {
-            j = 0
-            html += '</tr>'
-            continue
-        }
-        j++
+        document.querySelector('.class').appendChild(area)
+        document.querySelector('.class').appendChild(innerTable)
     }
-    html += '</table>'
-    document.querySelector('.class').innerHTML = html
 
     addEventToClass()
+}
+
+function createAcademicYearArea(academicYear) {
+    let tag = document.createElement('legend')
+    tag.innerHTML = academicYear
+
+    return tag
+}
+
+function createInnerTable() {
+    let table = document.createElement('table')
+    table.className = 'form-group'
+
+    return table
+}
+
+function createRow() {
+    return document.createElement('tr')
+}
+
+function createColumn() {
+    return document.createElement('td')
+}
+
+function createFacultyTag(faculty) {
+    let tag = document.createElement('div')
+    tag.innerHTML = faculty
+    tag.className = 'form-check form-check-inline'
+
+    return tag
+}
+
+function createCheckAllTag(academicYear, faculty) {
+    let tag = document.createElement('div')
+    tag.className = 'form-check form-check-inline'
+
+    let checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.className = academicYear + faculty + ` form-check-input`
+    checkbox.id = academicYear + faculty
+    checkbox.value = 'all'
+    checkbox.addEventListener('click', tickAllForClass)
+    checkbox.checked = true
+
+    let label = document.createElement('label')
+    label.htmlFor = academicYear + faculty
+    label.className = 'form-check-label text-nowrap'
+    label.innerHTML = 'Chọn tất cả'
+
+    tag.appendChild(checkbox)
+    tag.appendChild(label)
+
+    return tag
+}
+
+function createClassTag(_class, academicYear, faculty) {
+    let tag = document.createElement('div')
+    tag.className = 'form-check form-check-inline'
+
+    let checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    checkbox.className = academicYear + faculty + ` form-check-input`
+    checkbox.id = _class
+    checkbox.name = academicYear + faculty
+    checkbox.value = _class
+    checkbox.checked = true
+
+    let label = document.createElement('label')
+    label.htmlFor = _class
+    label.className = 'form-check-label text-nowrap'
+    label.innerHTML = _class
+
+    tag.appendChild(checkbox)
+    tag.appendChild(label)
+
+    return tag
+}
+
+function tickAllForClass() {
+    let checkBoxes = document.getElementsByName(this.classList[0])
+
+    for (let i = 0; i < checkBoxes.length; i++) {
+        if (checkBoxes[i].checked !== this.checked) {
+            checkBoxes[i].checked = this.checked
+            if (this.checked) {
+                selectedClass.push(checkBoxes[i].value)
+            }
+            else {
+                selectedClass.splice(selectedClass.lastIndexOf(checkBoxes[i].value), 1)
+            }
+        }
+    }
 }
 
 function addEventToClass() {
@@ -246,6 +343,7 @@ function addEventToClass() {
         checkBoxClass[i].addEventListener('change', function () {
             if (!this.checked && this.value !== 'all') {
                 selectedClass.splice(selectedClass.lastIndexOf(this.value), 1)
+
                 document.getElementsByClassName(this.name)[0].checked = false
             }
             else if (this.checked && this.value !== 'all') {
@@ -265,37 +363,78 @@ function addEventToClass() {
     }
 }
 
-function tickAllForClass(src) {
-    let checkBoxes = document.getElementsByName(src.classList[0])
+async function postData(data) {
+    const url = '../../../api-v2/manage/department_class_notification.php'
 
-    for (let i = 0; i < checkBoxes.length; i++) {
-        if (checkBoxes[i].checked !== src.checked) {
-            checkBoxes[i].checked = src.checked
-            if (src.checked) {
-                selectedClass.push(checkBoxes[i].value)
-            }
-            else {
-                selectedClass.splice(selectedClass.lastIndexOf(checkBoxes[i].value), 1)
-            }
-        }
+    const init = {
+        method: 'POST',
+        cache: 'no-cache',
+        body: JSON.stringify(data)
     }
+
+    const response = await fetch(url, init)
+    let responseAsJson = await response.json()
+
+    return responseAsJson
 }
 
-function sendNotification()
-{
-    // fetch('../../../api-v2/manage/department_class_notification.php', {
-    //     method: 'POST',
-    //     body: JSON.stringify(selectedClass)
-    // })
-    //     .then(function (response) {
-    //         return response.json()
-    //     })
-    //     .then(function (responseAsJson) {
-    //         allClass = responseAsJson
-    //     })
-    //     .catch(function (error) {
-    //         console.log('Looks like there was a problem: \n', error)
-    //     })
+function canPostData(data) {
+    for (const [field, fieldValue] of Object.entries(data.info)) {
+        if (fieldValue === '') {
+            alertify.error(`Trường "${fieldList[field]}" không được để trống!`)
+                .delay(3)
+                .dismissOthers()
 
-    console.log(JSON.stringify(selectedClass))
+            return false
+        }
+    }
+
+    if (selectedClass.length === 0) {
+        alertify.error('Checkbox chọn lớp không được để trống!')
+            .delay(3)
+            .dismissOthers()
+
+        return false
+    }
+
+    return true
+}
+
+async function trySendNotification() {
+    const data = {
+        info: {
+            title: title.value,
+            content: content.value,
+            typez: 'Type',
+            sender: sender
+        },
+        class_list: selectedClass
+    }
+
+    if (canPostData(data)) {
+        if (await postData(data) === 'OK') {
+            alertify.confirm('Thêm thông báo thành công!')
+                .setHeader('<i class="fas fa-info-circle"></i> Thông tin')
+                .setting({
+                    'labels':
+                        {
+                            ok: 'Tạo thông báo mới',
+                            cancel: 'Về trang chủ'
+                        },
+                    'defaultFocusOff': true,
+                    'maximizable': false,
+                    'movable': false,
+                    'pinnable': false,
+                    'onok': () => window.location.reload(),
+                    'oncancel': () => window.location.replace(('../../home/'))
+                })
+
+            document.getElementsByName('button')[0].removeEventListener('click', trySendNotification)
+        }
+        else {
+            alertify.error('Có lỗi đã xảy ra, hãy thử lại sau!')
+                .delay(3)
+                .dismissOthers()
+        }
+    }
 }
