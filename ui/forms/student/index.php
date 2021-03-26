0b0728af5117ef66c50d28add5f42602b09477bf
WRBKOR23 <haijdk2433@gmail.com>
2021-03-26 22:02:40 +0700

    create student push notification and push data ui

--- ui/forms/student/index.php
+++ ui/forms/student/index.php
@@ -6,23 +6,37 @@
     include_once $_SERVER['DOCUMENT_ROOT'] . "/utcapi/ui/shared/functions.php";
 
     shield();
-    shared_header('Thông báo cho sinh viên');
+    shared_header('Thông báo cho sinh viên', '
+      <!-- JQuery -->
+      <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+      <!-- AlertifyJS -->
+      <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+      <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+      <!-- AlertifyJS Theme -->
+      <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.rtl.min.css"/>
+      <link rel="stylesheet" href="../alert.css">
+    ');
 ?>
+
 <body>
   <div class="container">
 
     <?php shared_navbar(); ?>
 
     <main>
-      <form action="" autocomplete="off" enctype="multipart/form-data">
+      <form action="" autocomplete="off" enctype="multipart/form-data" onsubmit="return false">
 
           <?php shared_form(); ?>
 
         <div class=" mt-4">
           <legend>Tải tệp danh sách sinh viên nhân thông báo:</legend>
 
-          <input type="file" name="fileUpload"/>
-          <input type="submit" name="uploadclick" value="Upload"/>
+          <input id="fileUpload" type="file" name="fileUpload" multiple/>
+          <button type="button"
+                  class="btn btn-primary"
+                  id="confirm"
+                  name="upload">Xác nhận
+          </button>
 
         <div>
           <button type="button"
@@ -36,6 +50,7 @@
       </form>
     </main>
 </div>
+  <script src="script/script.js" type="module"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
           crossorigin="anonymous"></script>
---
+++ ui/forms/student/script/script.js
@@ -0,0 +1,124 @@
+import {postDataAndRaiseAlert, raiseEmptyFieldError} from '../../alerts.js'
+import {getSender, autoFillTemplate} from '../../shared.js'
+
+let sender
+let fileName = null
+const fieldList = {
+    title: 'Tiêu đề',
+    content: 'Nội dung',
+    typez: 'Loại thông báo'
+}
+
+const templateNoti = {
+    study: {
+        title: 'Học tập',
+        content: 'Nội dung thông báo học tập',
+        typez: 0
+    },
+    fee: {
+        title: 'Học phí',
+        content: 'Nội dung thông báo học phí',
+        typez: 1
+    },
+    extracurricular: {
+        title: 'Thông báo ngoại khóa',
+        content: 'Nội dung thông báo ngoại khóa',
+        typez: 2
+    },
+    social_payment: {
+        title: 'Chi trả xã hội',
+        content: 'Nội dung thông báo chi trả xã hội',
+        typez: 3
+    },
+    others: {
+        title: 'Thông báo khác',
+        content: 'Nội dung thông báo khác',
+        typez: 4
+    }
+}
+
+/*-----------------------------------------------*/
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+    document.getElementById('submit_btn').addEventListener('click', trySendNotification)
+    document.getElementById('template').addEventListener('change', fillForms)
+    document.getElementById('confirm').addEventListener('click', uploadFile)
+
+    sender = await getSender()
+})
+
+/*--------------------------------------*/
+
+async function uploadFile() {
+    if (fileUpload.files.length === 0) {
+        raiseEmptyFieldError('File Upload')
+        return
+    }
+
+    let response;
+    let formData = new FormData();
+    formData.append('flag', '0');
+
+    for (let i = 0; i < fileUpload.files.length; i++) {
+        formData.append('file'+1, fileUpload.files[i]);
+    }
+
+    let responseAsJson = await fetch('../../../worker/handle_file_upload.php', {
+        method: 'POST',
+        body: formData
+    });
+
+    response = responseAsJson.json();
+
+    if (response !== 'Failure') {
+        fileName = response
+        alert('The file has been uploaded successfully.');
+    }
+    else {
+        alert('Failed to upload files.');
+    }
+}
+
+/*---------------------------------------*/
+
+//  Display error if there are some unfulfilled fields
+function getInvalidField(data) {
+    for (const [field, fieldValue] of Object.entries(data.info)) {
+        if (fieldValue === '') {
+            return fieldList[field]
+        }
+    }
+
+    if (fileName === null) {
+        return 'File Upload'
+    }
+
+    return null
+}
+
+/*_________________________________________________*/
+
+async function trySendNotification() {
+    const data = {
+        info: {
+            title: $('#title').val(),
+            content: $('#content').val(),
+            typez: $('#type').val(),
+            sender: sender
+        },
+        file_name: fileName
+    }
+
+    const baseUrl = '../../../api-v2/manage/department_class_notification.php'
+
+    let madeRequest = await postDataAndRaiseAlert(baseUrl, data, getInvalidField)
+
+    if (madeRequest) {
+        document.getElementById('submit_btn').removeEventListener('click', trySendNotification)
+    }
+}
+
+function fillForms() {
+    autoFillTemplate(templateNoti[template.value])
+}
