c033837666b702ac8162fec319aa80dbc0260820
WRBKOR23 <haijdk2433@gmail.com>
2021-05-14 21:01:34 +0700

    test

--- api-v2/manage/cron_jobs/index.php
+++ api-v2/manage/cron_jobs/index.php
@@ -1,5 +1,6 @@
 <?php
-    include_once 'app/class/academic_year.php';
+    include_once '/app/class/academic_year.php';
+    include_once '/class/academic_year.php';
     include_once 'app/academic_year.php';
     include_once __DIR__ . '/class/academic_year.php';
     echo __DIR__ . '<br>';
