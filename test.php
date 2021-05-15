7b6626ee4b0bcde480872c3ada4eefcbf48b8fa1
WRBKOR23 <haijdk2433@gmail.com>
2021-05-15 23:18:53 +0700

    delete dump file

--- test.php
+++
@@ -1,4 +0,0 @@
-<?php
-//    file_put_contents('id_fix.txt', '12');
-   echo ini_get('max_execution_time');
-    //    echo file_get_contents('https://utcapi.herokuapp.com/api-v2/manage/cron_jobs/id_fix.txt');
