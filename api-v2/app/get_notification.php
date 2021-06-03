0454973019dbfc29124407d443d4bab854dc53e2
WRBKOR23 <haijdk2433@gmail.com>
2021-06-03 21:30:43 +0700

    add id_account to request get notification parameter

--- api-v2/app/get_notification.php
+++ api-v2/app/get_notification.php
@@ -2,12 +2,12 @@
     include_once dirname(__DIR__, 2) . '/config/db.php';
     include_once dirname(__DIR__, 2) . '/shared/functions.php';
     include_once dirname(__DIR__, 2) . '/class/notification_by_id_account.php';
-    include_once dirname(__DIR__, 2) . '/class/account.php';
     include_once dirname(__DIR__, 2) . '/class/data_version.php';
     set_error_handler('exceptions_error_handler');
 
     if ($_SERVER['REQUEST_METHOD'] == 'GET' &&
         isset($_GET['id_student']) &&
+        isset($_GET['id_account']) &&
         isset($_GET['version'])) {
 
         try {
@@ -19,11 +19,9 @@
             $app_data_version    = $_GET['version'];
 
             if ($latest_data_version != $app_data_version) {
-                $account    = new Account($connect);
-                $id_account = $account->getIDAccount($_GET['id_student']);
-
                 $notification_by_id_account = new NotificationByIDAccount($connect);
-                $response                   = $notification_by_id_account->getAll($id_account);
+                $response                   = $notification_by_id_account->getAll($_GET['id_account']);
+
                 if ($response['status_code'] == 200) {
                     $response['content']['data_version'] = $latest_data_version;
                 }
