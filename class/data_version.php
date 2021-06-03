8f31c3e499abb7c5822950b54e3da2d3fd8990df
WRBKOR23 <haijdk2433@gmail.com>
2021-06-03 16:26:05 +0700

    add class data version to specific version of data. add data version in response of get schedule, notification, score, exam schedule

---
+++ class/data_version.php
@@ -0,0 +1,58 @@
+<?php
+
+    class DataVersion
+    {
+        private const data_version_table = 'Data_Version';
+
+        private PDO $connect;
+        private string $id_student = '';
+
+        public function __construct (PDO $connect, $id_student)
+        {
+            $this->connect    = $connect;
+            $this->id_student = $id_student;
+        }
+
+        public function updateDataVersion ($type)
+        {
+            $sql_query = '
+                    UPDATE
+                        ' . self::data_version_table . ' 
+                    SET
+                        ' . $type . ' = ' . $type . ' + 1
+                    WHERE
+                        ID_Student = :id_student
+                    ';
+
+            try {
+                $stmt = $this->connect->prepare($sql_query);
+                $stmt->execute([':id_student' => $this->id_student]);
+
+            } catch (PDOException $error) {
+                throw $error;
+            }
+        }
+
+        public function getDataVersion ($type)
+        {
+            $sql_query =
+                'SELECT
+                    ' . $type . '
+                FROM
+                    ' . self::data_version_table . '
+                WHERE
+                    ID_Student = :id_student
+                ';
+
+            try {
+                $stmt = $this->connect->prepare($sql_query);
+                $stmt->execute([':id_student' => $this->id_student]);
+                $record = $stmt->fetch(PDO::FETCH_COLUMN);
+
+                return $record;
+
+            } catch (PDOException $error) {
+                throw $error;
+            }
+        }
+    }
