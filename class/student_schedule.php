428732ec3becac93b691f883c784026a6bc73244
WRBKOR23 <haijdk2433@gmail.com>
2021-06-05 23:53:35 +0700

    refactor code

--- class/student_schedule.php
+++ class/student_schedule.php
@@ -7,7 +7,6 @@
         private const schedule_table = 'Schedules';
         private const student_table = 'Student';
         private const participate_table = 'Participate';
-        private const module_table = 'Module';
 
         private string $student_id;
         private PDO $connect;
@@ -25,17 +24,16 @@
                     mdcls.Module_Class_Name, sdu.ID_Module_Class, 
                     sdu.ID_Room, sdu.Shift_Schedules, sdu.Day_Schedules
                 FROM
-                    ' . self::module_table . ' mc,
                     ' . self::schedule_table . ' sdu,
                     ' . self::student_table . ' stu,
                     ' . self::participate_table . ' par,
                     ' . self::module_class_table . ' mdcls
                 WHERE
-                    stu.ID_Student = :id_student AND
-                    par.ID_Student = :id_student AND
                     sdu.ID_Module_Class = par.ID_Module_Class AND
+                    par.ID_Student = :id_student AND
                     mdcls.ID_Module_Class = sdu.ID_Module_Class AND
-                    mc.ID_Module = mdcls.ID_Module
+                    stu.ID_Student = :id_student
+
                 ORDER BY
                     sdu.Shift_Schedules';
 
