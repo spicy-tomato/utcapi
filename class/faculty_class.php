773a4388e67b615695f0e6a40ef47a2970770db5
WRBKOR23 <haijdk2433@gmail.com>
2021-08-01 18:34:51 +0700

    test

--- class/faculty_class.php
+++ class/faculty_class.php
@@ -161,23 +161,23 @@
             $part_of_sql_data_version_2 = '';
 
             foreach ($student_list as &$student) {
-                $class_info              = $this->_getDataOfClass($student['ID_Class']);
-                $this->class_info_list[] = $class_info;
-                $student['ID_Class']     = $class_info[':id_class'];
+//                $class_info              = $this->_getDataOfClass($student['ID_Class']);
+//                $this->class_info_list[] = $class_info;
+//                $student['ID_Class']     = $class_info[':id_class'];
+//
+//                $sql_student_data[]  = $student['ID_Student'];
+//                $sql_student_data[]  = $student['Student_Name'];
+//                $sql_student_data[]  = $student['DoB'];
+//                $sql_student_data[]  = $student['ID_Class'];
+//                $part_of_sql_student .= '(?,?,?,?,null,null,null),';
 
-                $sql_student_data[]  = $student['ID_Student'];
-                $sql_student_data[]  = $student['Student_Name'];
-                $sql_student_data[]  = $student['DoB'];
-                $sql_student_data[]  = $student['ID_Class'];
-                $part_of_sql_student .= '(?,?,?,?,null,null,null),';
+                $sql_account_data[]  = $student['ID_Student'];
+                $sql_account_data[]  = password_hash($student['DoB'], PASSWORD_DEFAULT);
+                $part_of_sql_account .= '(?,null,?,null,0),';
 
-//                $sql_account_data[]  = $student['ID_Student'];
-//                $sql_account_data[]  = password_hash($student['DoB'], PASSWORD_DEFAULT);
-//                $part_of_sql_account .= '(?,null,?,null,0),';
-
-                $sql_data_version_data[]    = $student['ID_Student'];
-                $part_of_sql_data_version_1 .= '(?,0,0,0,0),';
-                $part_of_sql_data_version_2 .= '?,';
+//                $sql_data_version_data[]    = $student['ID_Student'];
+//                $part_of_sql_data_version_1 .= '(?,0,0,0,0),';
+//                $part_of_sql_data_version_2 .= '?,';
 
             }
 
